ec9e393715889239aae75b28608a22483aa96acb
anastasiiakudr <ankud.qa@gmail.com>
2024-11-11 15:20:27 -0800

    refactored code

--- cypress/e2e/jsonObjects.spec.js
+++ cypress/e2e/jsonObjects.spec.js
@@ -1,36 +1,31 @@
 it('JSON objects', () => {
+    // Open the homepage
     cy.openHomePage()
 
-    const simleObject = {'key': 'value', 'key2': 'value2'}
-
-    const simpleArrayObject = ['one', 'two', 'tree']
-
-    const arrayOfObject = [{'key': 'value'}, {'key1': 'value2'}, {'key3': 'value3'}]
-
-    const typesOfDate = {'string': 'this is string', 'number': 10}
-
+    // Define different types of objects and arrays
+    const simpleObject = { 'key': 'value', 'key2': 'value2' }
+    const simpleArrayObject = ['one', 'two', 'three']
+    const arrayOfObject = [{ 'key': 'value' }, { 'key1': 'value2' }, { 'key3': 'value3' }]
+    const typesOfData = { 'string': 'this is string', 'number': 10 }
+    
+    // Complex object with nested arrays and objects
     const mix = {
         'FirstName': 'Anastasiia',
         'LastName': 'Kud',
         'Age': 30,
         'Students': [
-            {
-                'firstName': 'Sara',
-                'lastName': 'Conor'
-            },
-            {
-                'firstName': 'Bruce',
-                'lastName': 'Willis'
-            }
+            { 'firstName': 'Sara', 'lastName': 'Conor' },
+            { 'firstName': 'Bruce', 'lastName': 'Willis' }
         ]
     }
 
-    console.log(simleObject.key2)    
-    console.log(simleObject['key2'])
-    console.log(simpleArrayObject[1])
-    console.log(arrayOfObject[2].key3)
-    console.log(mix.Students[1].lastName)
+    // Access and log various values
+    console.log(simpleObject.key2)              // Logs 'value2'
+    console.log(simpleObject['key2'])           // Logs 'value2'
+    console.log(simpleArrayObject[1])           // Logs 'two'
+    console.log(arrayOfObject[2].key3)          // Logs 'value3'
+    console.log(mix.Students[1].lastName)       // Logs 'Willis'
 
-    const lastNameOfSecondStudents = mix.Students[1].lastName
-
+    // Store a specific value in a variable
+    const lastNameOfSecondStudent = mix.Students[1].lastName
 })
